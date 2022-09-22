fdd9872c7ae60b9c1d9fe63eb507541496d407b6
zdindev <114016053+zdindev@users.noreply.github.com>
2022-09-22 20:14:11 +0300

    Add new problem solve

---
+++ printAndSum.js
@@ -0,0 +1,18 @@
+function printAndSum(start, end) {
+    
+    let result = '';
+    let sum = 0;
+
+    for (let i = start; i <= end; i++) {
+        
+        sum += i;
+        let current = String(i);
+        result += `${current} `
+
+    }
+        console.log(result);
+        console.log(`Sum: ${sum}`);
+
+}
+
+printAndSum(5, 10);
